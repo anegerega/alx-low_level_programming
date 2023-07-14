e2c31999723738bf98e66fa58cd570bc69a6dd74
anegerega <angelaegerega@yahoo.com>
2023-07-13 19:37:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,13 +10,6 @@
 
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
-
-	if (bytes_written == -1)
-	{
-	return errno;
-	}
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
