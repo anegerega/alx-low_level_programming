140b57e862e9e4388c5ef09f72947b124569a6bc
anegerega <angelaegerega@yahoo.com>
2023-07-14 17:39:56 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Main Function
+*
+*Return: 0 Always
+*
+*/
+int main(void)
+{
+char lowercase;
+char uppercase;
+
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+putchar(lowercase);
+putchar('\n');
+return (0);
+}
