aef1828a198827c0617290c1fc4ad29a3bca25b3
anegerega <angelaegerega@yahoo.com>
2023-07-14 17:51:38 -0700

    single digits on a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+*main - Prints all single digits on a new line.
+*
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+putchar('0');
+putchar('\n');
+
+putchar('1');
+putchar('\n');
+
+putchar('2');
+putchar('\n');
+
+putchar('3');
+putchar('\n');
+
+putchar('4');
+putchar('\n');
+
+putchar('5');
+putchar('\n');
+
+putchar('6');
+putchar('\n');
+
+putchar('7');
+putchar('\n');
+
+putchar('8');
+putchar('\n');
+
+putchar('9');
+putchar('\n');
+
+return (0);
+}
