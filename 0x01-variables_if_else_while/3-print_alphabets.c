48d34837d6b232d57621ec53eb4c5c21a7c05e91
anegerega <angelaegerega@yahoo.com>
2023-07-14 17:32:47 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,11 @@ int main(void)
 char lowercase;
 char uppercase;
 
-for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-{
+for(lowercase = 'a'; lowercase <= 'z'; lowercase++)
 putchar(lowercase);
-}
-for (uppercase = 'A'; uppercase <= 'z'; uppercase++)
-{
+for(uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 putchar(uppercase);
-}
+
 putchar('\n');
 return (0);
 }
