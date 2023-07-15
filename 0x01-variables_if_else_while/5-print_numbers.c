fc20ef5611101e2b4273857bd573a1fd64f231c3
anegerega <angelaegerega@yahoo.com>
2023-07-14 18:25:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - Prints all single digits of base ten on a new line.
+*
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(48 + n);
+}
+putchar('\n');
+return (0);
+}
