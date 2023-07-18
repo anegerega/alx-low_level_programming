1cd68fe528d29181802f2aaaa5ed273bf3e0eac5
anegerega <angelaegerega@yahoo.com>
2023-07-18 15:51:28 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs -a function that returns the absolute value of a number.
+* @num : checked parameter
+* Return: 0 Success
+*/
+int _abs(int num)
+{
+if (num < 0)
+{ 
+return (-num);
+}
+return (num);
+}
