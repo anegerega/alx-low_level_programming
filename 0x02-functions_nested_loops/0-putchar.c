402b44dfb1ed4c163084e43876a87f25a30f14fd
anegerega <angelaegerega@yahoo.com>
2023-07-18 10:43:13 -0700

    Correct directory name

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+* main - Program that prints _Putchar
+*
+* Return: 0 Success
+*/
+
+int main(void)
+{
+putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
